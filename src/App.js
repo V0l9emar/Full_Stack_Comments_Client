61462a6f44035713c56815fc552d99871b651ff7
Voldemar <63954046+V0l9emar@users.noreply.github.com>
2021-09-22 17:08:10 +0400

    Change CSS

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
 import Post from "./pages/Post";
-import Login from "./pages/Login";
 import Registration from "./pages/Registration";
+import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import Profile from "./pages/Profile";
+import ChangePassword from "./pages/ChangePassword";
+
 import { AuthContext } from "./helpers/AuthContext";
 import { useState, useEffect } from "react";
 import axios from "axios";
@@ -20,8 +22,10 @@ function App() {
 
   useEffect(() => {
     axios
-      .get("https://full-stack-comments.herokuapp.com//auth/auth", {
-        headers: { accessToken: localStorage.getItem("accessToken") },
+      .get("http://localhost:3001/auth/auth", {
+        headers: {
+          accessToken: localStorage.getItem("accessToken"),
+        },
       })
       .then((response) => {
         if (response.data.error) {
@@ -49,23 +53,21 @@ function App() {
             <div className="links">
               {!authState.status ? (
                 <>
-                  <Link to="/login">Login</Link>
-                  <Link to="/registration">Sign In</Link>
+                  <Link to="/login"> Login</Link>
+                  <Link to="/registration"> Registration</Link>
                 </>
               ) : (
                 <>
-                  <Link to="/">Home Page</Link>
-                  <Link to="/createpost">Create A Post</Link>
+                  <Link to="/"> Home Page</Link>
+                  <Link to="/createpost"> Create A Post</Link>
                 </>
               )}
             </div>
             <div className="loggedInContainer">
-              <h1> {authState.username}</h1>
+              <h1>{authState.username} </h1>
               {authState.status && <button onClick={logout}> Logout</button>}
             </div>
-            {/* <Link to="/post">Post</Link> */}
           </div>
-
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/createpost" exact component={CreatePost} />
@@ -73,6 +75,7 @@ function App() {
             <Route path="/registration" exact component={Registration} />
             <Route path="/login" exact component={Login} />
             <Route path="/profile/:id" exact component={Profile} />
+            <Route path="/changepassword" exact component={ChangePassword} />
             <Route path="*" exact component={PageNotFound} />
           </Switch>
         </Router>
